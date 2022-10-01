260093b38d93f3af020c837a2e10abe83438e222
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-10-02 02:38:42 +0300

    feat: add component 'Product'

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,13 +8,16 @@ interface ProductProps {
 export function Product({ product }: ProductProps) {
   const [details, setDetails] = useState(false);
 
+  const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
+  const btnClasses = ['py-2 px-4 border', btnBgClassName];
+
   return (
-    <div className='border py-2 px-4 rounded flex flex-col items-center mb-2'>
+    <div className='py-2 px-4 border rounded flex flex-col items-center mb-2'>
       <img src={product.image} className='w-1/6' alt={product.title} />
       <p>{product.title}</p>
       <p className='font-bold'>{product.price}</p>
       <button
-        className='py-2 px-4 border bg-yellow-400'
+        className={btnClasses.join(' ')}
         onClick={() => setDetails((prev) => !prev)}
       >
         {details ? "Hide Details" : "Show Details"}
@@ -22,6 +25,7 @@ export function Product({ product }: ProductProps) {
       {details && (
         <div>
           <p>{product.description}</p>
+          <p>Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
         </div>
       )}
     </div>
