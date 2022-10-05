fcd54500c908f15886dedb6e651dc8206338782c
Yauheniya Aniska <evgeniya.anisko@gmail.com>
2022-10-05 16:44:19 +0300

    feat: add context modal window and routing

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+export function AboutPage() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, odit repellendus accusantium esse atque nostrum alias pariatur. Nulla nostrum unde veritatis sequi possimus obcaecati sed commodi, necessitatibus sit nisi ipsum?
+    </>
+  )
+} 
