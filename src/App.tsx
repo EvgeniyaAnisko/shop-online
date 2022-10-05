e0a5adae22159db1619d24c691267b8584494484
Yauheniya Aniska <evgeniya.anisko@gmail.com>
2022-10-05 15:24:41 +0300

    feat: add modal window

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+import { CreateProduct } from "./components/CreateProduct";
 import { ErrorMessage } from "./components/ErrorMessage";
 import { Loader } from "./components/Loader";
+import { Modal } from "./components/Modal";
 import { Product } from "./components/Product";
 import { useProducts } from "./hooks/products";
+import { IProduct } from "./models";
 
 function App() {
+  const { loading, error, products, addProduct } = useProducts();
+  const [ modal, setModal ] = useState(false);
 
-  const { loading, error, products } = useProducts();
+  const createHandler = (product: IProduct) => {
+    setModal(false);
+    addProduct(product);
+  }
 
    return (
     <div className='container mx-auto max-w-2xl pt-5'>
@@ -14,6 +23,13 @@ function App() {
       {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
+      { modal && <Modal title="Create new product" onClose={() => setModal(false)}>
+        <CreateProduct onCreate={createHandler}/>
+      </Modal>}
+      <button 
+        className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
+        onClick={() => setModal(true)}
+        >+</button>
     </div>
   );
 }
--- src/hooks/products.ts
+++ src/hooks/products.ts
@@ -1,11 +1,16 @@
 import axios, { AxiosError } from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { IProduct } from "../models";
 
 export function useProducts() {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
+
+
+  function addProduct(product: IProduct) {
+    setProducts(prev => [...prev, product]);
+  }
 
   async function fetchProducts() {
     try {
@@ -26,5 +31,5 @@ export function useProducts() {
     fetchProducts();
   }, []);
 
-  return { products, error, loading };
+  return { products, error, loading, addProduct };
 }
