a84327fe75d6ffe60d6a5d32efdfc569a1d9ab73
Yauheniya Aniska <evgeniya.anisko@gmail.com>
2022-10-05 10:46:26 +0300

    feat: add fetch fakestoreapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Product } from './components/Product';
-import { products } from './data/products';
+import { Product } from "./components/Product";
+import { useProducts } from "./hooks/products";
 
 function App() {
-  return (
-    <div className="container mx-auto max-w-2xl pt-5">
-        <Product product={products[0]} />
-        <Product product={products[1]} />
+
+  const { loading, error, products } = useProducts();
+
+   return (
+    <div className='container mx-auto max-w-2xl pt-5'>
+      {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-600">{error}</p>}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
     </div>
   );
 }
